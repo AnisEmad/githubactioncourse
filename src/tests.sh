dc441e052caa9cb737a1eaad2cb99c9525438637
Anis Emad <anisemad123@gmail.com>
2025-09-30 13:53:01 +0300

    added some comments [skip ci]

--- src/tests.sh
+++ src/tests.sh
@@ -9,12 +9,13 @@ OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
 
 if [ "$OUTPUT" == "$EXPECTED" ]; then
 
+# outputs with check emojie first 
 echo -e "\u2705 Test passed "
 
 exit 0
 
 else 
-
+# output with x emojie first 
 echo -e "\u274c Test failed, the output didn't match the expected outcome!"
 
 exit 1
