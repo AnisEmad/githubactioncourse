46bb4d7066756a31a86ae77c96c5841209caec61
Anis Emad <anisemad123@gmail.com>
2025-09-30 13:50:28 +0300

    added check and failed emojies

--- src/tests.sh
+++ src/tests.sh
@@ -9,13 +9,13 @@ OUTPUT=$(node -e "console.log(require('./src/app')('Test'))")
 
 if [ "$OUTPUT" == "$EXPECTED" ]; then
 
-echo " Test passed "
+echo -e "\u2705 Test passed "
 
 exit 0
 
 else 
 
-echo "Test failed, the output didn't match the expected outcome!"
+echo -e "\u274c Test failed, the output didn't match the expected outcome!"
 
 exit 1
 
